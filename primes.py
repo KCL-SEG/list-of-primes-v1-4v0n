3691610c9d2e1245b7e4de32f2774fe8aa3de3c2
4v0n <navodhk@hotmail.com>
2022-10-06 00:10:59 +0100

    Created primes function

--- primes.py
+++ primes.py
@@ -3,4 +3,24 @@
 
 def primes(number_of_primes):
     list = []
+
+    num = 1
+
+    while (len(list) < number_of_primes):
+
+        num+=1
+        prime = True
+
+        for n in range(num):
+
+            if ((n > 1) and ((num % n) == 0)):
+                prime = False
+                break
+        
+        if prime:
+            list.append(num)
+
     return list
+
+
+primes(1)
